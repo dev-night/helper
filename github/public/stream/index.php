5bb8e9a3fcd7cbf9926e48a37fa538d9c0e4309e
kwetsch <37812582+kwetsch@users.noreply.github.com>
2018-04-10 12:28:53 +0200

    edit redirect to private server

--- github/public/stream/index.php
+++ github/public/stream/index.php
@@ -1,19 +1,3 @@
 <?php
-include "admin/includes/config.php";
-$result = $pdo->prepare("SELECT url FROM link");
-$result->execute();
-/* Fetch all of the remaining rows in the result set */
-while ($row = $result->fetch(PDO::FETCH_ASSOC))
-{
-    $youtube = $row['url'];
-}
-function Redirect($url, $permanent = false)
-{
-    if (headers_sent() === false)
-    {
-        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
-    }
-    exit();
-}
-
-Redirect($youtube, false);
+header("Location: https://tb-office.ml");
+die();
