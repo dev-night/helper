831dbeaa34d063da8f589d7bc843ffeb778c61a9
Niklas Heer <niklas.heer@gmail.com>
2018-08-31 21:43:20 +0200

    Update link for September event

--- github/public/meetup/index.php
+++ github/public/meetup/index.php
@@ -10,4 +10,4 @@ function Redirect($url, $permanent = false)
     exit();
 }
 
-Redirect('https://www.meetup.com/de-DE/dev_night/events/252970941/', false);
+Redirect('https://www.meetup.com/de-DE/dev_night/events/254267435/', false);
