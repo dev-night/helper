9c3110b43dc6b1145de543a7777a12876ceeed23
Niklas Heer <niklas.heer@gmail.com>
2018-10-23 15:32:38 +0200

    Update meetup link for November event

--- github/public/meetup/index.php
+++ github/public/meetup/index.php
@@ -10,4 +10,4 @@ function Redirect($url, $permanent = false)
     exit();
 }
 
-Redirect('https://www.meetup.com/de-DE/dev_night/events/254267781/', false);
+Redirect('https://www.meetup.com/de-DE/dev_night/events/255193266/', false);
