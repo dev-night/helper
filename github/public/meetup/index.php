19410686029429fe4cb0be8ad2c90dc83a98874b
Niklas Heer <niklas.heer@gmail.com>
2018-11-27 17:00:02 +0100

    Update meetup link for December event

--- github/public/meetup/index.php
+++ github/public/meetup/index.php
@@ -10,4 +10,4 @@ function Redirect($url, $permanent = false)
     exit();
 }
 
-Redirect('https://www.meetup.com/de-DE/dev_night/events/255193266/', false);
+Redirect('https://www.meetup.com/de-DE/dev_night/events/256747772/', false);
